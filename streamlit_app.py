74cb704e30c40670b8ea1eec57ea52f2f78cf5f3
RAKSHITADODDAMANI <130588095+RAKSHITADODDAMANI@users.noreply.github.com>
2025-11-07 16:10:26 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,7 +51,7 @@ if st.button("Generate Job Description"):
             """
 
             response = client.chat.completions.create(
-                model="mixtral-8x7b-32768",
+                model="llama3-8b-8192",
 
                 messages=[{"role": "user", "content": prompt}],
                 temperature=0.7,
